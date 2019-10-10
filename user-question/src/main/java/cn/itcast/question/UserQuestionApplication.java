3d43b11dca71e4e527d597b921d765e15d66d85e
carryzhang123 <1135484406@qq.com>
2019-10-11 00:22:22 +0800

    1

--- user-question/src/main/java/cn/itcast/question/UserQuestionApplication.java
+++ user-question/src/main/java/cn/itcast/question/UserQuestionApplication.java
@@ -1,4 +1,4 @@
-package cn.itcast.user;
+package cn.itcast.question;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +8,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @MapperScan("cn.itcast.cn.itcast.question.mapper")
 @EnableDiscoveryClient // 开启Eureka客户端发现功能
-public class UserApplication {
+public class UserQuestionApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
+        SpringApplication.run(UserQuestionApplication.class, args);
     }
 }
